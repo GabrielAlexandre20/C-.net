233504dd4c76f830cc69cd1f5d2c4e71eca8c37a
Gabriel Alexandre <94850511+GabrielAlexandre20@users.noreply.github.com>
2024-09-11 09:32:29 -0300

    Add files via upload
    Exemplo Singleton

---
+++ ExemploSingleton/ExemploSingleton/ConexaoDB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExemploSingleton
+{
+    public sealed class ConexaoDB
+    {
+        private static ConexaoDB instance = null;
+
+        private ConexaoDB()
+        {
+
+        }
+
+        public static ConexaoDB getInstance()
+        {
+
+            if (instance == null)
+            {
+                instance = new ConexaoDB();
+                Console.WriteLine("Criando um novo objeto de Conexao DB");
+            }
+            else
+            {
+                Console.WriteLine("Usando o objeto Conexao DB ja existente!");
+            }
+                
+
+            return instance;
+        }
+    }
+}
---
+++ ExemploSingleton/ExemploSingleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExemploSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConexaoDB con = ConexaoDB.getInstance();
+
+            ConexaoDB con2 = ConexaoDB.getInstance();
+
+            ConexaoDB con3 = ConexaoDB.getInstance();
+            
+        }
+    }
+}
